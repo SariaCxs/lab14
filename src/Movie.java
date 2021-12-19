5f4ea6b9e178c97809cc4dc730bd195b63fe8129
SariaCxs <11910233@mail.sustech.edu.cn>
2021-12-19 09:24:22 +0800

    original version

---
+++ src/Movie.java
@@ -0,0 +1,15 @@
+public class Movie {
+    String _title;
+    int _priceCode;
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+}
